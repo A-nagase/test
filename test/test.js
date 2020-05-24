31bd86b4a217c293e8f0a770eca6d1f89382da08
ayumu nagase <>
2020-05-24 15:00:13 +0900

    test

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-var msg = 'こんにちは!!!!! He\'s fine!';
+var msg1 = 'こんにちは!!!!! He\'s fine!';
 //window.alert(msg);
 
 //テンプレート文字列
@@ -30,5 +30,45 @@ console.log(x0);
 console.log(x1);
 console.log(others);
 
+//分割代入（オブジェクト）
+let book = {title : 'java',publish : '技術',price1 : 2680};
+let {price1,title,memo = 'なし'} = book;
+console.log(title);
+console.log(price1);
+console.log(memo);
 
+//条件演算子(?:)
+let x = 80;
+console.log((x >= 70) ? '合格' : '不合格');
 
+//ショートカット演算子
+let msg2 = '';
+msg2 = msg2 || 'Hello';
+console.log(msg2);
+
+let msg3 = '';
+msg3 = (msg3 === undefined ? 'Hello' : msg3);
+console.log(msg3);
+
+//delete演算子
+let ary = ['java','php','asp'];
+console.log(delete ary[0]);
+console.log(ary);
+
+//typeof演算子
+let num = 1;
+console.log(typeof num);
+
+let str1 = 'ハロー';
+console.log(typeof str1);
+
+let flag = true;
+console.log(typeof flag);
+
+let ary1 = ['a','b','c'];
+console.log(typeof ary1);
+
+let obj1 = {x:1,y:2};
+console.log(typeof obj1);
+
+//制御構文から
